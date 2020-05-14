d8e8d1d8c5ede3d66815d46aca8d5a33ca159272
Rijk <rijkdw@gmail.com>
2020-05-14 17:17:02 +0200

    Prettier and better data handling

--- front-end/lib/screens/home/chats.dart
+++ front-end/lib/screens/home/chats.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 class Chats extends StatelessWidget {
   
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _senderFont = const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+  final _messageReadFont = const TextStyle(fontSize: 14.0);
+  final _messageUnreadFont = const TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold);
+  final _messageTimeFont = const TextStyle(fontSize: 14.0);
 
-  var _users = [
-    { 'name': 'Rijk', 'last_message': 'Hello World!', 'last_message_time': '21:09', 'message': true },
-    { 'name': 'Ronaldo', 'last_message': 'Flutter is cool!', 'last_message_time': '10:42', 'message': false },
-    { 'name': 'Bill Gates', 'last_message': 'Nice app!', 'last_message_time': '13:14', 'message': true }
+  var _messagesArr = [
+    { 'name': 'Rijk', 'last_message': 'Hello World!', 'last_message_time': '21:09', 'unread': true },
+    { 'name': 'Ronaldo', 'last_message': 'Flutter is cool!', 'last_message_time': '10:42', 'unread': false },
+    { 'name': 'Bill Gates', 'last_message': 'Nice app!', 'last_message_time': '13:14', 'unread': true }
   ];
   
   @override
@@ -15,6 +18,7 @@ class Chats extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Chats Screen"),
+        backgroundColor: Color.fromRGBO(10, 10, 10, 1.0),
       ),
       body: _buildChatsList(),
     );
@@ -27,12 +31,19 @@ class Chats extends StatelessWidget {
         if (i.isOdd) return Divider();
 
         final index = i ~/ 2;
-        if (index < _users.length) return _buildRow(_users[index]);
+        if (index < _messagesArr.length) return _buildRow(_messagesArr[index]);
+        else return null;
       }
     );
   }
 
-  Widget _buildRow(userDict) {
+  Widget _buildRow(messageDict) {
+    
+    String _username = messageDict['name'];
+    String _message_text = messageDict['last_message'];
+    String _message_time = messageDict['last_message_time'];
+    bool _unread = messageDict['unread'];
+
     return Container(
       padding: const EdgeInsets.all(4),
       child: Row(
@@ -40,13 +51,16 @@ class Chats extends StatelessWidget {
 
           // profile picture
           Expanded(
-            flex: 13,
+            flex: 15,
             child: Container(
-              // padding: const EdgeInsets.only(right: 10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.face, color: Colors.orange)
+                  Icon(
+                    Icons.face,
+                    color: Colors.orange,
+                    size: 35,
+                  )
                 ]
               ),
             ),
@@ -59,8 +73,19 @@ class Chats extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(userDict['name'], style: _biggerFont, textAlign: TextAlign.left),
-                  Text(userDict['last_message'], textAlign: TextAlign.left),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      _username,
+                      style: _senderFont,
+                      textAlign: TextAlign.left
+                    ),
+                  ),
+                  Text(
+                    _message_text,
+                    style: _unread? _messageUnreadFont : _messageReadFont,
+                    textAlign: TextAlign.left
+                  ),
                 ]
               ),
             ),
@@ -74,8 +99,19 @@ class Chats extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Text(userDict['last_message_time'], style: _biggerFont, textAlign: TextAlign.left),
-                  Icon(userDict['message']? Icons.favorite: Icons.favorite_border),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      _message_time,
+                      style: _messageTimeFont,
+                      textAlign: TextAlign.left
+                    ),
+                  ),
+                  Icon(
+                    _unread? Icons.sms : null,
+                    color: Colors.orange,
+                    size: 20,
+                  ),
                 ]
               ),
             ),

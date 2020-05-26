6957257250db5888ecaed0b3d0a84ca810010d17
Jaakie <20774613@sun.ac.za>
2020-05-26 16:14:12 +0200

    Heroku working v1.0

--- Project_3/Proj_3/Proj_3/settings.py
+++ Project_3/Proj_3/Proj_3/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'app',
     'djoser',
     'rest_framework.authtoken',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -147,8 +148,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)

2dc4a60047b5bb189b1925f343556d23f2d73497
20688105 <20688105@sun.ac.za>
2020-06-01 17:48:55 +0300

    gave the app aids, but swiftly took care of it ;)

--- front-end/lib/screens/home/profile.dart
+++ front-end/lib/screens/home/profile.dart
@@ -173,8 +173,13 @@ class _ProfileWidgetState extends State<ProfileWidget> {
                             padding: const EdgeInsets.all(4),
                             child: InkWell(
                               onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => PostPage(posts[i])));
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => PostPage(
+                                      post: posts[i],
+                                    ),
+                                  ),
+                                );
                               },
                               child: PostCard(
                                 post: posts[i],
